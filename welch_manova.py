c84d7101fed63dcbc7faf0483957b44dccd6b534
jrudar <37217429+jrudar@users.noreply.github.com>
2020-05-13 22:06:05 -0400

    Add files via upload

--- welch_manova.py
+++ welch_manova.py
@@ -2,109 +2,124 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 from pandas import DataFrame
 
+from skbio.stats.distance import DissimilarityMatrix
+
 from deicode.matrix_completion import MatrixCompletion
 from deicode.preprocessing import rclr
 
-from numpy import asarray, power, copy, sqrt
+from numpy import asarray, power, copy, sqrt, float32, hstack
 from numpy.random import shuffle, choice
 
 from statsmodels.stats.multitest import multipletests
 
 def manova(D, y, n_perm):
 
+    """
+    Get the index of for every member of group A and group B and
+    permute if necessary
+    """
     y_set = asarray(list(set(y)))
 
-    k = y_set.shape[0]
-
     treatments = []
-    for i in range(k):
-        tmp = []
-        for j in range(y.shape[0]):
-            if y[j] == y_set[i]:
-                tmp.append(j)
+    if n_perm == 0:
+        y_copy = copy(y)
+        shuffle(y_copy)
 
-        treatments.append(asarray(tmp))
+        index_values = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y_copy)])
+
+        for treatment in y_set:
+            treatments.append(asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y_copy) 
+                                       if cluster_name == treatment]))
+
+    elif n_perm > 0:
+        index_values = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y)])
+
+        for treatment in y_set:
+            treatments.append(asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y) 
+                                       if cluster_name == treatment]))
+
+    D_obj = DissimilarityMatrix(D, index_values)
+
+    #Calculate k
+    k = y_set.shape[0]
 
     n = asarray([treatment.shape[0] for treatment in treatments])
 
-    #Calculate the treatment weights and the sum of squared distances
-    W = 0.0
-    treatment_w = []
-    SSD = []
-    for j in range(k):
-        treatment_index = treatments[j]
+    #Implementaion of Equations 4, 6, and 7
+    eqn_4 = 0.0
+    w_treatments = []
+    SSDs = []
+    for i in range(k):
+        treatment_ids = treatments[i]
 
-        n_j = n[j] 
+        n_i = n[i] 
 
-        SSD_j = 0.0
-        for p in range(0, treatment_index.shape[0] - 1):
-            p_index = treatment_index[p]
+        #Sum squared distances within treatment groups
+        SSD_i = (D_obj.within(treatment_ids).values[:,2].sum() / 2.0)
+        SSDs.append(SSD_i)
 
-            for q in range(p+1, treatment_index.shape[0]):
-                q_index = treatment_index[q]
-                SSD_j += power(D[p_index, q_index], 2)
+        #Equation 6 (Calculation of Within Treatment Variance)
+        eqn_6 = SSD_i / (n_i * (n_i - 1))
 
-        #Calculate weight for each treatment
-        w_j = (power(n_j, 2) * (n_j - 1)) / SSD_j
-        W += w_j
-        treatment_w.append(w_j)
+        #Equation 7 (Calculation of w_j)
+        eqn_7 = n_i / eqn_6
+        w_treatments.append(eqn_7)
 
-        SSD.append(SSD_j)
+        #Equation 4 (Sum of w_js)
+        eqn_4 += eqn_7
 
-    #This section calculates the weighted sum squared distances between treatments
-    wSSD = 0.0
-
+    #Implementation of equations 16 and 17
+    eqn_16_num = 0.0
     for i in range(0, k - 1):
-        #Retrieve data for the i-th treatment
-        w_i = treatment_w[i]
-        n_i = n[i]
-        SSD_i = SSD[i]
+        treatment_i = treatments[i]
 
+        #Retrieve data for the i-th treatment
+        w_i = w_treatments[i]
+        n_i = n[i]
+        SSD_i = SSDs[i]
+
+        #Section of Equation 17 for Treatment i
         wSSD_i = SSD_i / n_i
 
         for j in range(i+1, k):
-            #Retrieve data for the i-th treatment
-            w_j = treatment_w[j]
-            n_j = n[j]
-            SSD_j = SSD[j]
+            treatment_j = treatments[j]
 
+            #Retrieve data for the j-th treatment
+            w_j = w_treatments[j]
+            n_j = n[j]
+            SSD_j = SSDs[j]
+
+            #Section of Equation 17 for Treatment j
             wSSD_j = SSD_j / n_j
             
-            #Calculate all distances for the i-th and j-th treatment
-            index_values = asarray([ii for ii, cluster_name in enumerate(y) 
-                                    if cluster_name == y_set[i] 
-                                    or cluster_name == y_set[j]])
+            #Section of Equation 17 for Treatments i and j
+            SSD_ij = (D_obj.filter(hstack((treatment_i, treatment_j))).data.sum() / 2.0)
 
-            wSSD_ij = 0.0
-            for ii in range(0, index_values.shape[0]):
-                i_index = index_values[ii]
-                for jj in range(ii+1, index_values.shape[0]):
-                    j_index = index_values[jj]
+            wSSD_ij = SSD_ij / (n_i + n_j)
 
-                    wSSD_ij += power(D[i_index, j_index], 2)
+            #Summation for Equations 16 using Equation 17
+            eqn_16_num += w_i * w_j * (((n_i + n_j) * (wSSD_ij - (wSSD_i + wSSD_j))) / (n_i * n_j))
 
-            wSSD_ij = wSSD_ij / (n_i + n_j)
+    #Equantion 16
+    eqn_16 = eqn_16_num / eqn_4
 
-            wSSD += w_i * w_j * (((n_i + n_j) * (wSSD_ij - (wSSD_i + wSSD_j))) / (n_i * n_j))
+    #Impementation of Equation 5
+    sum_h = 0.0
+    for i, w_i in enumerate(w_treatments):
+        n_i = n[i]
 
-    wSSD = wSSD / W
+        eqn_5_num = power(((1-w_i) / eqn_4), 2)
+        
+        eqn_5 = eqn_5_num / (n_i - 1)
 
-    #Calculate the sum of all h_j
-    h_sum = 0.0
-    for j, w_j in enumerate(treatment_w):
-        n_j = n[j]
-
-        h_j = w_j / W
-        h_j = 1 - h_j
-        h_j = h_j / (n_j - 1)
-        h_sum += h_j
+        sum_h += eqn_5
 
     #Calculate W*d
-    W_num = wSSD / (k - 1)
+    W_num = eqn_16 / (k - 1)
 
     W_den_1 = 2 * (k - 2)
     W_den_2 = power(k, 2) - 1
-    W_den = 1 + (W_den_1 / W_den_2) * h_sum
+    W_den = 1 + (W_den_1 / W_den_2) * sum_h
 
     W_star = W_num / W_den
 
@@ -112,11 +127,8 @@ def manova(D, y, n_perm):
     n_greater = 0
     p = None
     if n_perm > 0:
-        y_copy = copy(y)
         for i in range(n_perm):
-            shuffle(y_copy)
-
-            W_perm = manova(D, y_copy, 0)[0]
+            W_perm = manova(D, y, 0)[0]
 
             if W_perm >= W_star:
                 n_greater += 1
@@ -127,92 +139,146 @@ def manova(D, y, n_perm):
 
     return W_star, p
 
-def ttest(D, y, A, B, n_perm):
+def ttest_ovr(D, y, A, n_perm):
 
-    #Get the index of for every member of group A and group B and
-    #permute if necessary
-    index_values = asarray([i for i, cluster_name in enumerate(y) 
-                            if cluster_name == A 
-                            or cluster_name == B])
+    """
+    Get the index of for every member of group A and group B and
+    permute if necessary
+    """
+    index_values = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y)])
 
-    A_index = asarray([i for i, cluster_name in enumerate(y) 
+    A_index = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y) 
                        if cluster_name == A])
 
-    B_index = asarray([i for i, cluster_name in enumerate(y) 
-                       if cluster_name == B])
+    B_index = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y) 
+                       if cluster_name != A])
 
-    if n_perm == 0:
-        shuffle(index_values)
+    if n_perm == 0: shuffle(index_values)
 
-        A_index = choice(index_values, A_index.shape[0], replace = False)
+    D_obj = DissimilarityMatrix(D, index_values)
 
-        B_index = asarray(list(set(index_values) - set(A_index)))
+    #Calculate the within-group and between-group differences
+    SSD_i = (D_obj.within(A_index).values[:,2].sum() / 2.0)
+    SSD_j = (D_obj.within(B_index).values[:,2].sum() / 2.0)
+    SSD_ij = (D_obj.data.sum() / 2.0)
 
-    treatments = [A_index, B_index]
-    n = [A_index.shape[0], B_index.shape[0]]
+    #Calculate the sample sizes
+    n_i = A_index.shape[0]
+    n_j = B_index.shape[0]
 
-    #Calculate the sum of squared distances and variance of each treatment
-    SSD = []
-    Var = []
-    for j, treatment in enumerate(treatments):
-        n_j = n[j] 
+    #Calculate the weighted sum of squared distances and variance
+    wSSD_i = SSD_i / n_i
+    wSSD_j = SSD_j / n_j
+    wSSD_ij = SSD_ij / (n_i + n_j)
 
-        SSD_j = 0.0
-        for p in range(0, treatment.shape[0] - 1):
-            i_index = treatment[p]
-            for q in range(p+1, treatment.shape[0]):
-                j_index = treatment[q]
-                SSD_j += power(D[i_index, j_index], 2)
-
-        #Calculate variance within each treatment
-        var_j = SSD_j / (n_j * (n_j - 1))
-        Var.append(var_j)
-
-        #Calculated the weighted SSD
-        SSD_j = SSD_j / n_j
-        SSD.append(SSD_j)
-
-    #Retrieve data for treatment A
-    n_i = n[0]
-    wSSD_i = SSD[0]
-
-    #Retrieve data for the j-th treatment
-    n_j = n[1]
-    wSSD_j = SSD[1]
-            
-    #Calculate for treatments A and B combined
-    wSSD_ij = 0.0
-    for ii in range(index_values.shape[0]):
-        i_index = index_values[ii]
-        for jj in range(ii+1, index_values.shape[0]):
-            j_index = index_values[jj]
-
-            wSSD_ij += power(D[i_index, j_index], 2)
-
-    wSSD_ij = wSSD_ij / (n_i + n_j)
+    var_i = SSD_i / (n_i * (n_i - 1))
+    var_j = SSD_j / (n_j * (n_j - 1))
 
     #Calculate T2w
-    T2w_num = ((n[0] + n[1]) / (n[0] * n[1])) * (wSSD_ij - wSSD_i - wSSD_j)
+    T2w_num = ((n_i + n_j) / (n_i * n_j)) * (wSSD_ij - wSSD_i - wSSD_j)
 
-    T2w_den = Var[0] + Var[1]
+    T2w_den = var_i + var_j
 
     T2w = T2w_num / T2w_den
 
-    #Calculate the effect size (Cohen's D and R2)
-    d2_num = T2w_num
+    #Calculate the effect size (Cohen's D)
+    if n_perm == 0:
+        d = None
 
-    d2_den = T2w_den / (n_i + n_j - 2)
+    elif n_perm > 0:
+        d2_num = T2w_num
 
-    d2 = d2_num / d2_den
+        d2_den = T2w_den / (n_i + n_j - 2)
 
-    d = sqrt(d2)
+        d2 = d2_num / d2_den
+
+        d = sqrt(d2)
 
     #Calculated the permuted statistic
     n_greater = 0
     p = None
     if n_perm > 0:
         for i in range(n_perm):
-            T_perm = ttest(D, y, A, B, 0)[0]
+            T_perm = ttest_ovr(D, y, A, 0)[0]
+
+            if T_perm >= T2w:
+                n_greater += 1
+
+        #The number 1 is added to both the numerator and denominator 
+        #since we already have at least 1 possible permutation (W_star)
+        p = (float(n_greater) + 1) / (float(n_perm) + 1)
+
+    return T2w, p, d
+
+def ttest_ovo(D, y, A, B, n_perm):
+
+    """
+    Get the index of for every member of group A and group B and
+    permute if necessary
+    """
+    index_values = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y)])
+
+    A_index = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y) 
+                       if cluster_name == A])
+
+    B_index = asarray(["Sample %s" %str(i) for i, cluster_name in enumerate(y) 
+                       if cluster_name == B])
+
+    D_obj = DissimilarityMatrix(D, index_values).filter(hstack((A_index, B_index)))
+
+    if n_perm == 0:
+
+        D_ss = D_obj.data
+
+        index_values = asarray(D_obj.ids)
+
+        shuffle(index_values)
+
+        D_obj = DissimilarityMatrix(D_ss, index_values)
+
+    #Calculate the within-group and between-group differences
+    SSD_i = (D_obj.within(A_index).values[:,2].sum() / 2.0)
+    SSD_j = (D_obj.within(B_index).values[:,2].sum() / 2.0)
+    SSD_ij = (D_obj.data.sum() / 2.0)
+
+    #Calculate the sample sizes
+    n_i = A_index.shape[0]
+    n_j = B_index.shape[0]
+
+    #Calculate the weighted sum of squared distances and variance
+    wSSD_i = SSD_i / n_i
+    wSSD_j = SSD_j / n_j
+    wSSD_ij = SSD_ij / (n_i + n_j)
+
+    var_i = SSD_i / (n_i * (n_i - 1))
+    var_j = SSD_j / (n_j * (n_j - 1))
+
+    #Calculate T2w
+    T2w_num = ((n_i + n_j) / (n_i * n_j)) * (wSSD_ij - wSSD_i - wSSD_j)
+
+    T2w_den = var_i + var_j
+
+    T2w = T2w_num / T2w_den
+
+    #Calculate the effect size (Cohen's D)
+    if n_perm == 0:
+        d = None
+
+    elif n_perm > 0:
+        d2_num = T2w_num
+
+        d2_den = T2w_den / (n_i + n_j - 2)
+
+        d2 = d2_num / d2_den
+
+        d = sqrt(d2)
+
+    #Calculated the permuted statistic
+    n_greater = 0
+    p = None
+    if n_perm > 0:
+        for i in range(n_perm):
+            T_perm = ttest_ovo(D, y, A, B, 0)[0]
 
             if T_perm >= T2w:
                 n_greater += 1
@@ -243,34 +309,70 @@ class WelchMANOVA():
     metric: str, default = "euclidean"
         The metric used to construct comparisons between samples.
 
+        The possible options are:
+            "compositional" - Constructs a distance matrix using Deicode
+
+            From Scikit-Learn: "cityblock", "cosine", "euclidean", "l1", "l2",
+            "manhattan", "braycurtis", "canberra", "chebyshev", "correlation",
+            "dice", "hamming", "jaccard", "kulsinski", "rogerstanimoto", 
+            "russellrao", "seuclidean", "sokalmichener", "sokalsneath",
+            "sqeuclidean", "yule"
+
+            "precomputed" - User supplied square distance matrix
+
     n_perm: int, default = 999
         The number of permutations
 
-    is_compositional: bool, default = True
-        Whether to construct a robust Aitchison distance matrix.
+    n_comp: int, default = 2
+        The number of components the matrix completion algorithm of the
+        Deicode package will extract. Only used if metric is 'compositional'.
+
+    max_iter: int, default = 10
+        The number of iterations the matrix completion algorithm of the
+        Deicode package will run for. Only use if metric is 'compositional'.
+
+    comp_method: str, default = "ovo"
+        If multiple treatment groups are present, this parameter specifies
+        if pairwise comparisons are conducted in a one-vs-one or one-vs-rest
+        manner.
+
+        Possible options are:
+            "ovo" - One vs. One
+            "ovr" - One vs. Rest
 
     alpha: float, default = 0.05
         Threshold for significance
 
     method: str, default = "b"
         Method which is used to adjust p-values if more than two comparisons
-        are made.
+        are made. Possible options are:
+            "b" - Bonferroni
+            "s" - Sidak
+            "h" - Holm
+            "hs" - Holm-Sidak
+            "sh" - Simes-Hochberg
+            "ho" - Hommel
+            "fdr_bh" - FDR Benjamini-Hochberg
+            "fdr_by" - FDR Benjamini-Yekutieli
+            "fdr_tsbh" - FDR Two-Stage Benjamini-Hochberg
+            "fdr_tsbky" - FDR Two-Stage Benjamini-Krieger-Yekutieli
+            "fdr_gbs" - FDR Adaptive Gavrilov-Benjamini-Sarkar
 
     """
     def __init__(self, 
                  metric = "euclidean",
                  n_perm = 999,
-                 is_compositional = False,
                  n_comp = 2,
                  max_iter = 10,
+                 comp_method = "ovo",
                  alpha = 0.05,
-                 method = "b"):
+                 method = "fdr_bh"):
 
         self.metric = metric
         self.n_perm = n_perm
-        self.is_compositional = is_compositional
         self.n_comp = n_comp
         self.max_iter = max_iter
+        self.comp_method = comp_method
         self.alpha = alpha
         self.method = method
 
@@ -290,15 +392,19 @@ class WelchMANOVA():
         """
 
         #Transform into a distance matrix
-        if self.is_compositional == False:
-            D = pairwise_distances(X, 
-                                   metric = self.metric)
+        if self.metric != "compositional":
+            if self.metric != "precomputed":
+                D = pairwise_distances(X, 
+                                       metric = self.metric).astype(float32)
 
-        elif self.is_compositional == True:
+            else:
+                D = X
+
+        elif self.metric == "compositional":
             X_trf = rclr(DataFrame(X).astype(float))
 
             D = MatrixCompletion(self.n_comp, 
-                                 self.max_iter).fit(X_trf).distance
+                                 self.max_iter).fit(X_trf).distance.astype(float32)
 
         #Save data
         self.D = D
@@ -307,7 +413,7 @@ class WelchMANOVA():
 
         #Calculate permuted statistic
         if len(set(self.y)) > 2:
-            W_star, p_value = manova(self.D, 
+            W_star, p_value = manova(power(self.D, 2), 
                                      self.y,
                                      self.n_perm)
 
@@ -339,15 +445,29 @@ class WelchMANOVA():
         self.test_statistics = []
         self.raw_p_values = []
         self.effect_size = []
-        for i in range(0, len(y_set) - 1):
-            for j in range(i+1, len(y_set)):
-                raw_pairwise = ttest(self.D, 
-                                        self.y,
-                                        y_set[i],
-                                        y_set[j],
-                                        self.n_perm)
 
-                self.comparision.append("%s-%s" %(y_set[i], y_set[j]))
+        if self.comp_method == "ovo":
+            for i in range(0, len(y_set) - 1):
+                for j in range(i+1, len(y_set)):
+                    raw_pairwise = ttest_ovo(power(self.D, 2), 
+                                             self.y,
+                                             y_set[i],
+                                             y_set[j],
+                                             self.n_perm)
+
+                    self.comparision.append("%s-%s" %(y_set[i], y_set[j]))
+                    self.test_statistics.append(raw_pairwise[0])
+                    self.raw_p_values.append(raw_pairwise[1])
+                    self.effect_size.append(raw_pairwise[2])
+
+        elif self.comp_method == "ovr":
+            for i in range(0, len(y_set)):
+                raw_pairwise = ttest_ovr(power(self.D, 2), 
+                                         self.y,
+                                         y_set[i],
+                                         self.n_perm)
+
+                self.comparision.append("%s-Rest" %y_set[i])
                 self.test_statistics.append(raw_pairwise[0])
                 self.raw_p_values.append(raw_pairwise[1])
                 self.effect_size.append(raw_pairwise[2])
@@ -355,8 +475,8 @@ class WelchMANOVA():
         #Adjust for multiple comparisons
         if len(y_set) > 2:
             self.adjusted_p_values = multipletests(self.raw_p_values,
-                                                    self.alpha,
-                                                    self.method)
+                                                   self.alpha,
+                                                   self.method)[1]
 
             self.table_result = DataFrame(data = [self.test_statistics,
                                                     self.raw_p_values,
@@ -378,4 +498,6 @@ class WelchMANOVA():
                                             columns = self.comparision).transpose()
 
         print(self.table_result)
+
+
 
